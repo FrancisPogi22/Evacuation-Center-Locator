71273ae1cb044b532b59bbaedcabbddf1912f131
margallostephen <spmargallo@gmail.com>
2023-09-15 19:23:42 +0800

    comment vite header

--- resources/views/userpage/hazardReport/manageHazardReport.blade.php
+++ resources/views/userpage/hazardReport/manageHazardReport.blade.php
@@ -4,7 +4,7 @@
 <head>
     @include('partials.headPackage')
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
-    @vite(['resources/js/app.js'])
+    {{-- @vite(['resources/js/app.js']) --}}
 </head>
 
 <body>
